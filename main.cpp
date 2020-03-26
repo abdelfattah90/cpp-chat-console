bc2131b8cb1143b0730e83e9e7854e9abfc355b1
unknown <ahmedomar1007@gmail.com>
2020-03-27 01:00:21 +0200

    input output loop array

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+
 int main() {
-  cout << "Hello C++!";
+  cout << "Welcome to this chat" << endl;
+
+  cout << "What's your name ?" << endl;
+  string name;
+  cin >> name;
+  cout << endl;
+
+  cout << "How old are you " << name << endl;
+  int age;
+  cin >> age;
+  cout << endl;
+
+  cout << "What's your favor color, " << name << endl;
+  string color[5] = {"white", "black", "purple", "blue", "green"};
+  for(int i = 0; i < 4; i++) {
+    cout << color[i] << endl;
+  }
+  string fColor;
+  cin >> fColor;
+  cout << endl;
   return 0;
 }
